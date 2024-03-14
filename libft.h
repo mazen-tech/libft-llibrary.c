7d10035aab6a16a906b872571b6bc7c5c9306381
Mazen Tarek <104716013+mazen-tech@users.noreply.github.com>
2024-03-14 09:42:10 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -18,8 +18,8 @@
 size_t	ft_strlen(const char *src);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t n);
-char	ft_toupper(char c);
-char	ft_tolower(char c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
