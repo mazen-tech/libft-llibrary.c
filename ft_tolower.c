84333deb1dea61817554c18f952b59561a62494f
Mazen Tarek <104716013+mazen-tech@users.noreply.github.com>
2024-03-14 09:26:24 +0100

    Update ft_tolower.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-char	ft_tolower(char c)
+int	ft_tolower(int c)
 {
 	if (c <= 'Z' && c >= 'A')
 	{
